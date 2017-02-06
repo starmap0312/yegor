ccb1f3a922dbd684ac2ab7d17d7ed97472999038
Kuan-Yu Chen <starmap0312@gmail.com>
2017-02-06 12:56:30 +0800

    add more

--- 4_dependency_injection_containers_are_code_polluters.java
+++ 4_dependency_injection_containers_are_code_polluters.java
@@ -42,5 +42,44 @@ public class Budget {
 // but almost any object will want to encapsulate some knowledge (i.e. state)
 
 
-// DI (Dependency Injection) Container
+// DI (Dependency Injection) Container: ex. Google Guice
+//
+// example
+import javax.inject.Inject;
 
+public class Budget {
+
+    private final DB db;
+
+    @Inject                  // the constructor is annotated with @Inject
+    public Budget(DB data) {
+      this.db = data;
+    }
+
+    public long total() {
+        return this.db.cell("SELECT SUM(cost) FROM ledger");
+    }
+}
+
+// not instantiate Budget through the new operator, but use the injector instead
+public class App {
+
+    public static void main(String[] args) {
+        // configure a container somewhere when the application starts
+        Injection injector = Guice.createInjector(
+            new AbstractModule() {
+                @Override
+                public void configure() {
+                    this.bind(DB.class).toInstance(new Postgres("jdbc:postgresql:5740/main"));
+                }
+            }
+        );
+        // the injection automatically finds out that in order to instantiate a Budget it has to provide an argument for its constructor
+        // it will use an instance of class Postgres, which we instantiated in the injector
+        Budget budget = injector.getInstance(Budget.class);
+        System.out.println("Total is: " + budget.total());
+    }
+}
+// why is it bad?
+//   additional complexity: adds more lines to the code base, or even more files (ex. XML)
+// use object composition instead
