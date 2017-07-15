11fe725a6e2766a5d988d5c457a97e9f68efd1a0
kuanyu <starmap0312@gmail.com>
2017-07-15 11:33:23 +0800

    add more

---
+++ 0_yegor_design.java
@@ -0,0 +1,56 @@
+// 1) jcabi-http
+// 1.1) class design
+package com.jcabi.http;
+    public interface Request  // responsible to construct RequestURI/RequestBody instances and provides Response instance 
+        /* the public constant literals related to this interface that can be used by the outside world            */
+        String GET = "GET";   // the public constant literals defined in this interface
+        String POST = "POST"; //   ex. Request.GET, ex. Request.POST, etc.
+        /* the public methods declared in this interface, the contract that all of its implementations must follow */
+        RequestURI uri();
+        RequestBody body();
+        RequestBody multipartBody();
+        Response fetch() throws IOException;
+
+    public interface RequestBody
+        Request back();      // for backtrace to its Request instance
+    public interface RequestURI
+        Request back();      // for backtrace to its Request instance
+    public interface Response
+        Request back();      // for backtrace to its Request instance
+
+    public interface Wire    // responsible to construct Response instances
+        Response send(...);
+
+package com.jcabi.http.request;
+    final class BaseRequest implements Request               // a class is either final or abstract
+        public RequestURI uri() {                            // BaseRequest provides its own RequestURI implementation
+            return new BaseRequest.BaseURI(this, ...);
+        }
+        public RequestBody body() {                          // BaseRequest provides its own RequestBody implementation
+            return new BaseRequest.FormEncodedBody(this, ...);
+        }
+        public RequestBody multipartBody() {                 // BaseRequest provides another RequestBody implementation
+            return new BaseRequest.MultipartFormBody(this, ...);
+        }
+        public Response fetch() throws IOException {         // BaseRequest delegates construction of Response to its wire
+            return this.wire.send(this, ...);
+        }
+        // note, all the above construction pass the parameter this (as one of its characteristics of the instance)
+        //   so that the receiver of the instances can backtrace to this Request instance
+        //   Request -> RequestURI
+        //           -> RequestBody
+        //           -> Response
+        //   ex. Request request = new BaseRequest(new Wire(...));
+        //       RequestURI uri = request.uri();      // uri is an instance of BaseRequest.BaseURI
+        //       RequestBody body = request.body();   // body is an instance of BaseRequest.FormEncodedBody
+        //       Response response = request.fetch(); // the implementation of Response depends on the request's wire
+        //       uri.back();                          // returns req
+        //       body.back();                         // returns req
+        //       response.back();                     // returns req
+
+        /* BaseRequest defines its own RequestURI/RequestBody implementations as inner classes                   *
+         *   private to outside world: because classes that receive these instances should program to interfaces *
+         *   not implementations, so they need not to know the implementation details                            */
+        private static final class BaseURI implements RequestURI { ... }
+        private static final class FormEncodedBody implements RequestBody { ... }
+        private static final class MultipartFormBody implements RequestBody { ... }
