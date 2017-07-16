bca1d936ab1719dfdacb2fbee73f0f42ffbba9ed
kuanyu <starmap0312@gmail.com>
2017-07-16 20:52:26 +0800

    add more

--- 0_checked_vs_unchecked_exceptions_the_debate_is_not_over.java
+++ 0_checked_vs_unchecked_exceptions_the_debate_is_not_over.java
@@ -4,14 +4,14 @@
 //                       -> Exception -> RuntimeException -> NullPointerException (unchecked exception)
 //                                    -> InterruptedException                     (checked exception) 
 //                                    -> ...
-//
+
 // you can throw everything that is Throwable
 // 1) if you throw checked excpetions (i.e. not Error/RuntimeException)
 //    the compiler will check for you and any method beyond must be declared with "throws" keyword
 // 2) if a method catches it without re-throwing, it doesn't need the throws keyword (not recommended)
 // 3) python only has unchecked excpeitons (no checked exceptions at compile-time)
 //    i.e. you don't know if a method may throw any exception written by the programmer
-//
+
 // unchecked vs. checked exceptions:
 // 1) unchecked exceptions:
 //    exceptions that extend RuntimeException/Error
@@ -23,31 +23,35 @@
 //    i.e. checked exceptions need to be explicitly catched or rethrown
 //
 // why is unchecked exception bad?
-//   hiding the fact that a method may fail
-//   often, a method is too complex and want to keep some exceptions "hidden" (i.e. unchecked)
-//   try to refactor method so that it is responsible for one thing and throws checked exception if fails
+// 1) hiding the fact that a method may fail
+// 2) the method may be too complex and want to keep some exceptions "hidden" (i.e. unchecked)
+//    try to refactor method so that it is responsible for one thing and throws checked exception if fails
 //
 // why is checked exception good?
-//   unlike unchecked exceptions, we can't ignore failures: need to try/catch somewhere
-//   every method is either "safe" (throws nothing) or "unsafe" (throws Exception)
-//     to be safe, the method needs to handle the excpetion by itself
-//     otherwise, the method is not safe and the client have to worry about the safety
-//
+// 1) unlike unchecked exceptions, we can't ignore failures
+//    need to try/catch somewhere
+// 2) every method is either "safe" (throws nothing) or "unsafe" (throws Exception)
+//    to be safe, the method needs to handle the excpetion by itself
+//    otherwise, the method is not safe and the client have to worry about the safety
+
 // rule of thumbs:
 // 1) always use checked exceptions and never use unchecked exceptions
 //    all methods are declared either as "safe" (throws nothing) or "unsafe" (throws Exception)
-// 2) never use exceptions for flow control: use only Exception without any sub-types
-//    we don't need multiple exception types, if we don't use exceptions to control flow
-// 3) never recover from exceptions
-//    i.e. no fail slow: never catch without rethrowing
-//
-// example.
+// 2) never use exceptions for flow control
+// 3) use only Exception without any sub-types
+//    we don't need multiple exception types, because we don't use exceptions to control flow
+// 4) never recover from exceptions
+//    i.e. never catch without rethrowing (no fail slow)
+
+// example 1:
 // (bad design: use Exception subtypes for control flow)
 //   use an Exception subtype for memory allocation errors (unchecked exception)
 //   ex. OutOfMemoryException
 // (good design: introduce some memory manager for control flow, writing methods to check the memory)
 //   use a method to tell whether heap is big enough for the next operation
 //   ex. bigEnough()
+
+// example 2:
 // (bad design: fail slow, recover from the exception)
 try {
     save(file, data);
