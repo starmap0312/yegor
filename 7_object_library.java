6867c35ef23e785f47f9463aa195899b5384da3e
kuanyu <starmap0312@gmail.com>
2017-07-16 15:49:11 +0800

    add more

---
+++ 7_object_library.java
@@ -0,0 +1,12 @@
+// Library Oriented Architecture (LOA)
+// 1) it is a set of principles and methodologies for designing and developing software in the form of reusable libraries
+// 2) it dictates the boundaries of a library by exposing functionality through a set of public interfaces (APIs)
+// 3) it promotes separation of concerns and programming to interfaces not implementations
+// 4) it promotes practices similar to Modular Programming and encourages the maintenance of internal libraries
+//
+// principles of LOA
+// 1) a software library implementation must be constrained to only one domain
+// 2) all domain specific software libraries must be maintained and supported with separate life-cycles
+// 3) a software library that needs to use concepts from a different domain than the one it belongs to
+//    must use/interact with the library corresponding to that domain
+//
---
+++ 7_object_pool.java
@@ -0,0 +1,35 @@
+// Object pool
+// 1) a creational design pattern that keeps a set of initialized objects ready to use
+//    i.e. using an object pool rather than allocating and destroying them on demand
+//    ex. database connections, socket connections, threads, large graphic objects like fonts/bitmaps
+// 2) advantages:
+//    object pool significantly improves performance
+// 3) disadvantages:
+//    object pool complicates object lifetime
+//    objects obtained from and returned to a pool are not actually created or destroyed at this time
+//      therefore, it requires extra care in implementation
+// 4) used when:
+//    working with a large number of objects expensive to instantiate, and
+//    each object is only needed for a short period of time
+// 5) implementation details:
+//    a) the client requests an object from the pool and performs operations on the returned object
+//       when the client finishes, it returns the object to the pool rather than destroying it
+//    b) if no objects are present in the pool, a new object/item is created and returned
+//    c) if resources are limited, a maximum number of objects is specified
+//    d) object pooling may require resources, ex. memory or network sockets
+//       therefore, it is preferable that the number of instances in the pool is low (not required)
+//    e) handling of empty pools:
+//       if it fails to provide an object, then return an error to the client
+//       allocating a new object increases the size of the pool
+//    f) in a multithreaded environment
+//       a pool may block the client until another thread releases an object to the pool
+//
+// object pool with smart pointers, ex. C++: smart pointers
+//   in the constructor of the smart pointer, an object can be requested from the pool
+//   in the destructor of the smart pointer, the object can be released back to the pool
+// object pool for garbage collected languages (no destructors):
+//   do it manually/explicitly
+//     requesting an object from the factory
+//     returning the object by calling a dispose method
+//   use with try/finally construct or context manager
+//     using a finalizer is not a good idea as there are no guarantees on when the finalizer will be run
