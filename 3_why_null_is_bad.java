be4664dcd834f19e2cce25f5ad03047bfa64ef5e
kuanyu <starmap0312@gmail.com>
2017-08-20 21:35:36 +0800

    add more

--- 3_why_null_is_bad.java
+++ 3_why_null_is_bad.java
@@ -1,8 +1,8 @@
-// NULL references
-//   NULL refernece is a clear indicator of code smell
-//   NULL references is an inheritance of procedural programming, ex. C
-//   use Null Objects / Exceptions instead
-// in java, null reference can be assigned to any object type
+// null references
+//   null refernece is a clear indicator of code smell
+//   null references is an inheritance of procedural programming, ex. C
+//   use null Objects / Exceptions instead
+// in java, null reference can be assigned to any Object type
 //   ex. String str = null;
 //   ex. Integer i  = null;
 //   ex. Double num = null;
@@ -10,48 +10,53 @@
 //   ex. int n = null;      ==> compile error
 //   ex. double num = null; ==> compile error
 //
+// why use exception or null objects?
+//   the client code is greatly simplified (no ad-hoc handling of null reference)
+//
 // example: InputStream, resource that needs to be open and close explicitly
 //
-// (bad design: use of NULL reference)
+// (bad design: use of null reference)
 InputStream input = null;
 try {
     input = url.openStream();    // open the resource, may throw IOException if unable to open
     //... ...                    // read the stream, may throw IOException
-} catch (IOException ex) {
+} catch (IOException ex) {       // throw exception if openStream() or read stream fails
     throw new RuntimeException(ex);
 } finally {
-    if (input != null) {         // finally close the resource if not null
+    if (input != null) {         // ad-hoc handling in the finally block (i.e. close the resource if not null)
         input.close();
     }
 }
 // why is it bad?
-//   requires Ad-hoc Error Handling of the Null reference
+//   requires ad-hoc error handling of the null reference
 
-// (good design: no NULL reference, throw exceptions)
+// (good design: no null reference, but throw exceptions instead)
 final InputStream input;
 try {
     input = url.openStream();    // open the resource, may throw IOException
-} catch (IOException ex) {
+} catch (IOException ex) {       // throw exception if openStream() fails
     throw new RuntimeException(ex);
 }
 
 try {
     //... ...                    // read the stream, may throw IOException
-} catch (IOException ex) {
+} catch (IOException ex) {       // throw exception if read stream fails
     throw new RuntimeException(ex);
 } finally {                      // finally close the resource
     input.close();
 }
 // why is it good?
-//   fail fast: hiding failure from the client (easy to identify problems and bugs)
+// 1) fail fast, exception are thrown immediately, easier to identify problems and bugs
+// 2) no hiding of failure from the client
+// 3) no ad-hoc handling of null reference
 
 // example:
-// 1) use of NULL references: need Ad-hoc Error Handling in client code
-//    whenever you get an object, you need to check whether it is NULL or a valid object reference
-//    if you forget to check, a NullPointerException (NPE) may break execution in runtime
+// 1) use of null references: need ad-hoc error handling in client code
+//    whenever you get an object, you need to check whether it is null or a valid object reference
+//    if you forget to check, a nullPointerException (NPE) may break execution in runtime
 //    thus, your logic becomes polluted with multiple checks and if/then/else blocks
 //
-// ex. the client code in procedural programming 
+// ex. the client code requires ad-hoc handling of null reference 
 Employee employee = dept.getByName("Jeffrey");                 // may return null
 Employee employee = dept.getByNameOrNullIfNotFound("Jeffrey"); // better naming
 
@@ -70,15 +75,15 @@ public Employee getByNameOrNullIfNotFound(String name) {
     return new Employee(id);
 }
 
-// 2) in OOP, there are two alternatives to NULL references:
-//    a) use NULL object with common behaviors or throw exceptions
+// 2) in OOP, there are two alternatives to null references:
+//    a) use null object with common behaviors
 //    b) throw Exceptions directly: fail fast
 //
-// a) implementation of NULL objects
+// a) implementation of null objects
 public Employee getByName(String name) {
     int id = database.find(name);
     if (id == 0) {
-        return Employee.NOBODY;                                // return NULL object
+        return Employee.NOBODY;                                // return null object
     }
     return Employee(id);
 }
@@ -87,23 +92,23 @@ public Employee getByName(String name) {
 public Employee getByName(String name) {
     int id = database.find(name);
     if (id == 0) {
-        throw new EmployeeNotFoundException(name);             // throw an exception
+        throw new EmployeeNotFoundException(name);             // throw an exception (fail fast)
     }
     return Employee(id);
 }
-// if getByName fails, it should raise an exception: i.e. fail fast, hiding failure from the client
 
-// the client code is simplified (not ad-hoc handling)
+// the client code is simplified (no ad-hoc handling)
+//   getByName either throws exception or returns a null object that has a common transferTo() behavior (ex. exception)
 dept.getByName("Jeffrey").transferTo(dept2);
 
 
-// 3) NULL reference has performance advantanges? No.
-// a) NULL reference
+// 3) null reference has performance advantanges? No.
+// a) null reference
 // ex. a real example of using Map in Java: only one search in Map is required
 //     use null reference to require only one search
 public Employee getByName(String name) {
     Employee employee = employees.get("Jeffrey"); // use of get() method of Map interface in Java: one search
-    if (employee == NULL) {                       // use of null reference to avoid second search
+    if (employee == null) {                       // use of null reference to avoid second search
         throw new EmployeeNotFoundException();
     }
     return employee;
@@ -130,22 +135,22 @@ public Employee getByName(String name) {
 // example of null object
 // a) fail fast: make your code as fragile as possible, letting it break immediately and when necessary
 //    i.e. hiding this failure from its client (easier to debug, don't make it fail slowly)
-//    don't use NULL references, instead, use NULL objects or raise exceptions
+//    don't use null references, instead, use null objects or raise exceptions
 // b) make your methods extremely demanding as to the data they manipuate
 //    complain by throwing exceptions, if the provided data is not sufficient or wrong
-//    you can also use a NULL object instead
+//    you can also use a null object instead
 //   
-// implementation of NULL object in getByName()
+// implementation of null object in getByName()
 public Employee getByName(String name) {
     int id = database.find(name);
     Employee employee;
     if (id == 0) {
-        employee = new Employee() {                   // a anonymous class of NULL object
+        employee = new Employee() {                   // a anonymous class of null object
             public String name() {                    // define common behavior on some methods
                 return "anonymous";
             }
             public void transferTo(Department dept) { // throws an exception on other methods
-                                                      // i.e. Null object should throw exceptions on all other calls
+                                                      // i.e. null object should throw exceptions on all other calls
                 throw new AnonymousEmployeeException("I can't be transferred, I'm anonymous");
             }
         };
@@ -154,10 +159,11 @@ public Employee getByName(String name) {
     }
     return employee;
 }
-// note: Null Object exposes common behavior and throws exceptions on all other method calls
+// note: null Object exposes common behavior on non-critical behaviors, and
+//       it throws exceptions on all other critical method calls
 
-// the client code is rather simple
+// the client code becomes rather simple
 employee = dept.getByName("Unknown")
-System.out.println(employee.name()); // common behavior
-employee.transferTo(dept2);          // throw an exception
+System.out.println(employee.name()); // common behavior, not critical
+employee.transferTo(dept2);          // critical behavior, may throw an exception
 
