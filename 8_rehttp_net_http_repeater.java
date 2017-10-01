6f71edbd735cf850d7e5471405fd93aab9d32918
kuanyu <starmap0312@gmail.com>
2017-10-01 14:36:17 +0800

    add more

---
+++ 8_rehttp_net_http_repeater.java
@@ -0,0 +1,29 @@
+// HTTP Repeater
+// problem:
+// 1) a web app is supposed to receive HTTP requests from GitHub (i.e. GitHub webhooks), but that app is down
+// 2) GitHub gets an HTTP error, and never sends the request again
+//    there is no way to receive it again once the app is back up
+//
+// solution:
+// a service mesh between GitHub and web app, to accept HTTP requests and repeat them later if not delivered
+// 1) GitHub sends all webhook PUT/POST requests to the ReHTTP server, which stores them in a database (ex. AWS DynamoDB)
+// 2) ReHTTP attempts to deliver them immediately
+//    if something goes wrong and the server HTTP response code is not in the 200-299 range,
+//      the next attempt is made in about an hour
+//    then it retries every hour for about a day
+//    if all attempts fail, it abandons the request and that's it
+// 3) you know how many requests were processed successfully over the last 24 hours, and how many of them failed
+//
+// Service Mesh
+// 1) a service mesh is an infrastructure layer for making service-to-service communication reliable
+//    ex. Ticketmaster, Paypal, etc. uses a service mesh
+// 2) it is typically implemented as an array of lightweight network proxies deployed alongside application code
+//    the application needs not to be aware the existence of a sercie mesh
+// 3) the service mesh is a networking model that sits at a layer of abstraction above TCP/IP
+//    it must also be capable of handling network failures
+//    like the TCP stack which abstracts the mechanics of reliably delivering bytes between network endpoints,
+//      the service mesh abstracts the mechanics of reliably delivering requests between services
+//    Unlike the TCP, the service mesh can provide a uniform visibility and control into the application at runtime
+// 4) reliably delivering requests in a cloud native application is incredibly complex
+//    it may use  a wide array of powerful techniques: 
+//    ex. circuit-breaking, latency-aware load balancing, consistent (advisory) service discovery, retries, and deadlines
