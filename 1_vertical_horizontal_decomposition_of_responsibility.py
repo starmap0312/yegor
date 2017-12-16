a1db86db15a4040b04da4b6d2bad297e8f968acf
kuanyu <starmap0312@gmail.com>
2017-12-16 20:24:52 +0800

    add more

--- 1_vertical_horizontal_decomposition_of_responsibility.py
+++ 1_vertical_horizontal_decomposition_of_responsibility.py
@@ -1,12 +1,10 @@
 import datetime
 # vertical vs. horizontal decomposition of responsibility
-#
-#   vertical decomposition is better
+#   vertical decomposition is better!
 #
 # example:
 #
 # (bad design: before decomposition)
-#
 # a class responsible for two things: formatting the text and writing the formated text to a file
 class FileTimedLog(object):
 
@@ -20,13 +18,14 @@ class FileTimedLog(object):
 FileTimedLog('/tmp/log.txt').put('Message\n')
 
 # why is it bad?
-#   not re-usable and extensible
+# 1) it is not re-usable
+# 2) it is not extensible
 #
-# (good design: after horizontal decomposition)
-#   horizontally decomposition increases complexity as client has more dependencies and points of contact
+# (not so good design: horizontal decomposition of responsibilities)
+# horizontal decomposition increases complexity as client has more dependencies and points of contact
 #
-#   class Script --> class Log
-#                --> class Line
+# class Script --> class Log
+#              --> class Line
 #
 # the class is responsible for only writing the line to a file
 class FileLog(object):
@@ -46,7 +45,9 @@ class TimedLine(object):
     def __str__(self):
         return datetime.datetime.now().strftime("%B %d, %Y: ") + self.line
 
-# the client class: need to know both classes (collaborators), i.e. class Log and class Line, to provide its service
+# the client class needs to know both classes (collaborators) to provide its service
+# i.e. Script --> TimedLine
+#             --> FileLog
 class Script(object):
 
     def write(self, text, filepath):
@@ -56,14 +57,14 @@ class Script(object):
 
 script = Script().write('Message\n', '/tmp/log.txt')
 
-# why is it not good enough?
-#   the Scirpt class depends on two collaborators, i.e. class TimedLine and class FileLog
+# why is it not so good?
+# 1) the Scirpt class depends on two collaborators, i.e. class TimedLine and class FileLog
 #
 #
-# (better design: after vertical decomposition)
-#   vertical decomposition decreases complexity
+# (good design: vertical decomposition of responsibilities)
+# vertical decomposition decreases complexity
 #
-#   class Script --> class TimedLog --> class Log
+# Script --> TimedLog --> Log
 #
 # the class is responsible for only writing the line to a file (no change)
 class FileLog(object):
@@ -84,6 +85,7 @@ class TimedLog(object):
         self.origin.put(datetime.datetime.now().strftime("%B %d, %Y: ") + text)
 
 # the client class has only one entry point to Log object
+# i.e. Script --> TimedLog --> Log
 class Script(object):
 
     def write(self, text, log):
@@ -94,5 +96,6 @@ class Script(object):
 script = Script().write('Message\n', FileLog('/tmp/log.txt'))
 
 # why is it good?
-#   the reponsibilities are well separated, so it is easier to reuse and extend
-#   the client class depends on one class, i.e. class TimedLog, one single entry point
+# 1) the reponsibilities are still well separated, so it is easier to reuse and extend
+# 2) the client class depends on one class, i.e. class TimedLog, one single entry point
+
